7a9cea29980c98bcd0a86edfe6c9fb8a5cdd93ef
Darmawan Efendi <darmawan.efendi@gmail.com>
2019-10-10 10:01:41 +0700

    enhancement unit test

--- test/test-pages.test.js
+++ test/test-pages.test.js
@@ -25,43 +25,43 @@ describe("GET /", function() {
             });
     });
 
-    // it("it should test has status code 200", function(done) {
-    //     request(app)
-    //         .get("/test")
-    //         .expect(200)
-    //         .end(function(err, res){
-    //             if (err) done(err);
-    //             done();
-    //         });
-    // });
-    //
-    // it("it should test has hello world", function(done) {
-    //     request(app)
-    //         .get("/test")
-    //         .expect('Hello World!!!')
-    //         .end(function(err, res){
-    //             if (err) done(err);
-    //             done();
-    //         });
-    // });
-    //
-    // it("it should test has status code 200", function(done) {
-    //     request(app)
-    //         .get("/test2")
-    //         .expect(200)
-    //         .end(function(err, res){
-    //             if (err) done(err);
-    //             done();
-    //         });
-    // });
-    //
-    // it("it should test has hello world", function(done) {
-    //     request(app)
-    //         .get("/test2")
-    //         .expect('Hello World!!!')
-    //         .end(function(err, res){
-    //             if (err) done(err);
-    //             done();
-    //         });
-    // });
+    it("it should test has status code 200", function(done) {
+        request(app)
+            .get("/test")
+            .expect(200)
+            .end(function(err, res){
+                if (err) done(err);
+                done();
+            });
+    });
+
+    it("it should test has hello world", function(done) {
+        request(app)
+            .get("/test")
+            .expect('Hello World!!!')
+            .end(function(err, res){
+                if (err) done(err);
+                done();
+            });
+    });
+
+    it("it should test has status code 200", function(done) {
+        request(app)
+            .get("/test2")
+            .expect(200)
+            .end(function(err, res){
+                if (err) done(err);
+                done();
+            });
+    });
+
+    it("it should test has hello world", function(done) {
+        request(app)
+            .get("/test2")
+            .expect('Hello World!!!')
+            .end(function(err, res){
+                if (err) done(err);
+                done();
+            });
+    });
 });
